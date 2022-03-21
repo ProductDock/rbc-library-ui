195e51d749b92db2e2e8fe096763c881b3a1b835
NenadBecanovicPD <nenad@nenads-MacBook-Pro.local>
2022-03-21 13:16:38 +0100

    Added test for SectionTitle component

---
+++ src/components/Section/SectionTitle/SectionTitle.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import SectionTitle from ".";
+
+test("should render section title with text correclty", () => {
+  render(<SectionTitle title="All books" numberOfBooks={1} />);
+  const linkElement = screen.getByText("All books (1)");
+  expect(linkElement).toBeInTheDocument();
+});
