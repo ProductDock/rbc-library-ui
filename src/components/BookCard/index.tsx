808aefd2c25ec5ef8a387d12f0d39e362250b8b7
natasa <natasa@natasas-MacBook-Pro.local>
2022-03-23 15:43:28 +0100

    Shown real books instead dummy books

--- src/components/BookCard/index.tsx
+++ src/components/BookCard/index.tsx
@@ -4,13 +4,13 @@ import "./BookCard.css";
 type Props = {
   title: string;
   author: string;
-  imageUrl: string;
+  cover: string;
 };
 
-const BookCard = ({ title, author, imageUrl }: Props) => {
+const BookCard = ({ title, author, cover }: Props) => {
   return (
     <div className="book-box">
-      <BookCover imageUrl={imageUrl} />
+      <BookCover imageUrl={cover} />
       <div className="text-box">
         <p className="title-box">{title}</p>
         <p className="author-box">{author}</p>
--- src/components/BookCollection/index.tsx
+++ src/components/BookCollection/index.tsx
@@ -1,65 +1,24 @@
+import { useEffect } from "react";
+import { useBooksContext } from "../../store/books/BooksContext";
 import BookCard from "../BookCard";
 import "./bookCollection.css";
 
 const BookCollection = () => {
-  const books = [
-    {
-      title: "Book title",
-      author: "John Doe",
-      imageUrl:
-        "https://www.oreilly.com/library/view/java-program-design/9781484241431/images/978-1-4842-4143-1_CoverFigure.jpg",
-    },
-    {
-      title: "Book2",
-      author: "John Smith",
-      imageUrl:
-        "https://s3.us-east-2.amazonaws.com/shulph-dev-shelf/books/covers/9781789130690.jpg",
-    },
-    {
-      title: "Book title",
-      author: "John Doe",
-      imageUrl:
-        "https://www.oreilly.com/library/view/java-program-design/9781484241431/images/978-1-4842-4143-1_CoverFigure.jpg",
-    },
-    {
-      title: "Book2",
-      author: "John Smith",
-      imageUrl:
-        "https://s3.us-east-2.amazonaws.com/shulph-dev-shelf/books/covers/9781789130690.jpg",
-    },
-    {
-      title: "Book title",
-      author: "John Doe",
-      imageUrl:
-        "https://www.oreilly.com/library/view/java-program-design/9781484241431/images/978-1-4842-4143-1_CoverFigure.jpg",
-    },
-    {
-      title: "Book2",
-      author: "John Smith",
-      imageUrl:
-        "https://s3.us-east-2.amazonaws.com/shulph-dev-shelf/books/covers/9781789130690.jpg",
-    },
-    {
-      title: "Book title",
-      author: "John Doe",
-      imageUrl:
-        "https://www.oreilly.com/library/view/java-program-design/9781484241431/images/978-1-4842-4143-1_CoverFigure.jpg",
-    },
-    {
-      title: "Book2",
-      author: "John Smith",
-      imageUrl:
-        "https://s3.us-east-2.amazonaws.com/shulph-dev-shelf/books/covers/9781789130690.jpg",
-    },
-  ];
+  const { books, findAllBooks } = useBooksContext();
+
+  useEffect(() => {
+    findAllBooks?.();
+  }, []);
+
   return (
     <div className="all-books">
       {books.map((book) => {
         return (
           <BookCard
+            key={book.id}
             title={book.title}
             author={book.author}
-            imageUrl={book.imageUrl}
+            cover={book.cover}
           />
         );
       })}
