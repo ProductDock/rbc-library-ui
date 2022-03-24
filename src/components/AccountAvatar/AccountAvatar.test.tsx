fe50955d5e087f7b342ab91ca5dfdbb182a8e776
nenadbecanovicpd <nenad.becanovic@productdock.com>
2022-03-24 13:30:08 +0100

    Added tests for AccountAvatar component

---
+++ src/components/AccountAvatar/AccountAvatar.test.tsx
@@ -0,0 +1,19 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import AccountAvatar from ".";
+
+describe("AccountAvatar component", () => {
+  test("should render account avatar and check for dropdown menu button", () => {
+    render(<AccountAvatar />);
+    const accountAvatar = screen.getByLabelText("Account settings");
+    expect(accountAvatar).toBeInTheDocument();
+  });
+
+  test("should render account avatar and show dropdown menu on button click", () => {
+    render(<AccountAvatar />);
+    const button = screen.getByRole("button");
+
+    fireEvent.click(button);
+
+    expect(screen.getByRole("menu")).toBeInTheDocument();
+  });
+});
