a6812d3e289f7883119be527640bdecaf9ce0088
natasa <natasa@natasas-MacBook-Pro.local>
2022-03-25 15:39:48 +0100

    Changed BookCard test to work with BookContextProvider

--- src/components/BookCard/BookCard.test.tsx
+++ src/components/BookCard/BookCard.test.tsx
@@ -1,8 +1,32 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "../../App";
+import { Book } from "../../store/books/Types";
+import * as BooksContext from "../../store/books/BooksContext";
+import * as AuthContext from "../../store/auth/AuthContext";
 
-test("renders test page and finds title", () => {
+const books: Book[] = [
+  { id: 1, author: "John Doe", cover: "Cover", title: "Book2" },
+];
+
+beforeEach(() => {
+  jest.spyOn(AuthContext, "useAuthContext").mockImplementation(() => ({
+    isLoggedIn: true,
+    loaded: true,
+    userProfile: {
+      name: "test",
+      imageUrl: "http://test.com",
+      email: "test@test.com",
+    },
+  }));
+});
+
+test("renders test page and finds title", async () => {
+  jest.spyOn(BooksContext, "useBooksContext").mockImplementation(() => ({
+    books,
+    loading: false,
+    error: null,
+  }));
   render(<App />);
   const bookTitle = screen.getByText("Book2");
   expect(bookTitle).toBeInTheDocument();
