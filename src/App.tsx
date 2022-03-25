5c40d79fce14ddac9fc0a5d3cc3933f4f5318934
nenadbecanovicpd <nenad.becanovic@productdock.com>
2022-03-25 11:01:17 +0100

    Remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import PageRouter from "./router";
-<<<<<<< HEAD
-import logo from "./img/pd-logo.svg";
 import AuthContextProvider from "./store/auth/AuthContext";
-import AccountAvatar from "./components/AccountAvatar";
 
 function App() {
   return (
@@ -14,23 +11,9 @@ function App() {
         <NavBar />
         <div className="main-div">
           <PageRouter />
-          <div className="account-avatar">
-            <AccountAvatar />
-          </div>
         </div>
       </Router>
     </AuthContextProvider>
-=======
-
-function App() {
-  return (
-    <Router>
-      <NavBar />
-      <div className="main-div">
-        <PageRouter />
-      </div>
-    </Router>
->>>>>>> 84ad5fb (Refactored navbar to be a separate component and fixed fonts)
   );
 }
 
