2c62b74b4ecdca322c6482b7b3b4e36e4fdfb4e4
Dusan <dusan.petrovic@productdock.com>
2022-03-29 15:20:21 +0200

    Removed unnecessary xsrfToken form data field from google api call

--- src/services/GoogleService.tsx
+++ src/services/GoogleService.tsx
@@ -4,7 +4,6 @@ const OAUTH2_REVOKE_API = "https://accounts.google.com/o/oauth2/revoke";
 
 export const revokeOauth2Token = (token: string) => {
   const data = new FormData();
-  data.append("xsrfToken", "");
   data.append("token", token);
 
   return axios.post(OAUTH2_REVOKE_API, data);
