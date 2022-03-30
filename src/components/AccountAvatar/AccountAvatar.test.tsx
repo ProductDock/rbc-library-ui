99aaf0f4abd2f06c1697dfb99d4cbf444b7d2b32
Dusan <dusan.petrovic@productdock.com>
2022-03-30 16:00:18 +0200

    Added test for sign out

--- src/components/AccountAvatar/AccountAvatar.test.tsx
+++ src/components/AccountAvatar/AccountAvatar.test.tsx
@@ -1,21 +1,49 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import AccountAvatar from ".";
+import App from "../../App";
+import * as AuthContext from "../../store/auth/AuthContext";
 
 describe("AccountAvatar component", () => {
   test("should render account avatar and check for dropdown menu button", () => {
-    render(<AccountAvatar />);
+    render(<App />);
     const accountAvatar = screen.getByLabelText("Account settings");
     expect(accountAvatar).toBeInTheDocument();
   });
 
   test("should render account avatar and show dropdown menu on button click", () => {
-    render(<AccountAvatar />);
-    const button = screen.getByRole("button");
-
+    render(<App />);
+    const button = screen.getByTestId("avatar-button");
     userEvent.click(button);
-
     expect(screen.getByRole("menu")).toBeInTheDocument();
+  });
+
+  test("should call sign out method and redirect to landing url", async () => {
+    const signOutMock = jest.fn().mockResolvedValue({});
+
+    jest.spyOn(AuthContext, "useAuthContext").mockImplementation(() => ({
+      signOut: signOutMock,
+      isLoggedIn: true,
+      loaded: true,
+      userProfile: {
+        name: "test",
+        imageUrl: "http://test.com",
+        email: "test@test.com",
+        googleAccessToken: "1234567",
+      },
+    }));
+
+    render(<App />);
+
+    const avatarButton = await screen.findByTestId("avatar-button");
+    userEvent.click(avatarButton);
+
+    const signOutButton = screen.getByTestId("sign-out-button");
+    userEvent.click(signOutButton);
+
+    expect(signOutMock).toBeCalledTimes(1);
+
+    const landingText = await screen.findByText("Landing page");
+    expect(landingText).toBeTruthy();
   });
 });
