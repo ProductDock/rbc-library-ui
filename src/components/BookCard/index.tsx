30949d882cf964dd74fd690ea3e3581b04c0f33f
vukomanv-pd <vladimir.vukoman@productdock.com>
2022-03-30 11:13:26 +0200

    Author name reformat and tooltip on hover

--- src/components/BookCard/index.tsx
+++ src/components/BookCard/index.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Tooltip, Typography } from "@mui/material";
 import BookCover from "../BookCover";
 import "./BookCard.css";
 
@@ -13,12 +13,14 @@ const BookCard = ({ title, author, cover }: Props) => {
     <div className="book-box" data-testid="book-box">
       <BookCover imageUrl={cover} />
       <div className="text-box">
-        <p className="title-box">
+        <span className="title-box">
           <Typography>{title}</Typography>
-        </p>
-        <p className="author-box">
-          <Typography>{author}</Typography>
-        </p>
+        </span>
+        <span className="author-box">
+          <Tooltip title={author}>
+            <Typography>{author}</Typography>
+          </Tooltip>
+        </span>
       </div>
     </div>
   );
--- src/components/BookCollection/index.tsx
+++ src/components/BookCollection/index.tsx
@@ -13,15 +13,14 @@ const BookCollection = () => {
 
   return (
     <div className="all-books">
+      <BookCard
+        key={1}
+        title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia et possimus deleniti maxime placeat autem dolores facere tempora expedita? Fugiat!"
+        author="Lorem ipsum dolor sit amfdgdgfgffghhfhet consectetur adipisicing elit. Quia et possimus deleniti maxime placeat autem dolores facere tempora expedita? Fugiat!"
+        cover=""
+      />
       {books.map((book) => {
-        return (
-          <BookCard
-            key={book.id}
-            title={book.title}
-            author={book.author}
-            cover={book.cover}
-          />
-        );
+        return <BookCard key={book.id} title={book.title} author={book.author} cover={book.cover} />;
       })}
     </div>
   );
