cbee0c652366588a8619ef503f3befaf0d2aa849
Dusan <dusan.petrovic@productdock.com>
2022-03-30 15:59:56 +0200

    Modified account avatar to work with real user data

--- src/components/AccountAvatar/index.tsx
+++ src/components/AccountAvatar/index.tsx
@@ -7,33 +7,49 @@ import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
 import Logout from "@mui/icons-material/Logout";
 import { Typography } from "@mui/material";
-import userImage from "../../loggedInUser.png";
 import "./AccountAvatar.css";
+import { useNavigate } from "react-router";
+import { useAuthContext } from "../../store/auth/AuthContext";
+import { routes } from "../../constants/routes";
 
 const AccountAvatar = () => {
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const { userProfile, signOut } = useAuthContext();
+  const navigate = useNavigate();
+
   const open = Boolean(anchorEl);
+
   const handleClick = (event: any) => {
     setAnchorEl(event.currentTarget);
   };
+
   const handleClose = () => {
     setAnchorEl(null);
   };
-  const signOut = () => {
-    // To be replaced with logout handling
-    handleClose();
+
+  const handleSignOut = () => {
+    signOut?.()
+      .then(() => navigate(routes.WELCOME))
+      .then(() => handleClose());
   };
-  return (
+
+  return userProfile ? (
     <>
       <Box>
         <Tooltip title="Account settings">
           <IconButton
+            data-testid="avatar-button"
             onClick={handleClick}
             aria-controls={open ? "account-menu" : undefined}
             aria-haspopup="true"
             aria-expanded={open ? "true" : undefined}
           >
-            <img className="credentials-image-navbar" src={userImage} alt="" />
+            <img
+              referrerPolicy="no-referrer"
+              className="credentials-image-navbar"
+              src={userProfile?.imageUrl}
+              alt=""
+            />
           </IconButton>
         </Tooltip>
       </Box>
@@ -46,21 +62,26 @@ const AccountAvatar = () => {
         anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
       >
         <MenuItem>
-          <img className="credentials-image" src={userImage} alt="" />
+          <img
+            referrerPolicy="no-referrer"
+            className="credentials-image"
+            src={userProfile?.imageUrl}
+            alt=""
+          />
           <div className="d-flex flex-column">
             <div className="d-flex flex-row user-name">
               <span>
-                <Typography>Name LastName</Typography>
+                <Typography>{userProfile?.name}</Typography>
               </span>
             </div>
             <div className="d-flex flex-row">
               <span className="email">
-                <Typography>example@productdock.com</Typography>
+                <Typography>{userProfile?.email}</Typography>
               </span>
             </div>
           </div>
         </MenuItem>
-        <MenuItem onClick={signOut}>
+        <MenuItem onClick={handleSignOut} data-testid="sign-out-button">
           <ListItemIcon>
             <Logout fontSize="small" />
           </ListItemIcon>
@@ -68,7 +89,7 @@ const AccountAvatar = () => {
         </MenuItem>
       </Menu>
     </>
-  );
+  ) : null;
 };
 
 export default AccountAvatar;
