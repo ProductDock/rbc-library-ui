296de76a25c5b566092861a9a4fc37cf646ed8ad
natasa <natasa@natasas-MacBook-Pro.local>
2022-03-31 16:04:30 +0200

    added LandingPage

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,30 @@
+import "./LandingPage.css";
+import { Typography } from "@mui/material";
+import logo from "../img/pd-logo.svg";
+import LoginButton from "../components/LoginButton";
+
 const LandingPage = () => {
   return (
-    <div className="main-div">
+    <div className="landing-container">
       <div className="img-div">
-        <span>Ovde ide slika</span>
+        <img className="landing-img" src="images/landing-cover.png" alt="" />
       </div>
       <div className="content-div">
-        <span>ProductDock...</span>
+        <img src={logo} alt="logo" />
+
+        <div className="landing-text">
+          <div className="welcome-text">
+            <Typography className="small-font">Welcome to</Typography>
+            <Typography className="large-font">
+              <b>ProductDock</b> Library
+            </Typography>
+          </div>
+
+          <Typography className="small-font">
+            Please use ProductDock Google account
+          </Typography>
+        </div>
+        <LoginButton />
       </div>
     </div>
   );
