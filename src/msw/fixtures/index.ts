96805cd2d90cfd3fb20bccd2060930b9191024e9
Natasa Tutus <natasa.tutus@productdock.com>
2022-04-15 09:39:37 +0200

    changed URL route for BookDetails in testing data

--- src/msw/fixtures/index.ts
+++ src/msw/fixtures/index.ts
@@ -1,3 +1,1 @@
 export { default as BooksFixture } from "./BooksFixture.json";
-export { default as BookFixture } from "./BookFixture.json";
-export { default as BookFixture2 } from "./BookFixture2.json";
--- src/msw/handlers.ts
+++ src/msw/handlers.ts
@@ -1,5 +1,5 @@
 import { rest } from "msw";
-import { BooksFixture, BookFixture, BookFixture2 } from "./fixtures";
+import { BooksFixture } from "./fixtures";
 
 export const BOOKS_URL = `*/books`;
 
@@ -7,7 +7,5 @@ export const handlers = [
 
   rest.get(BOOKS_URL, (req, res, ctx) => res(ctx.status(200, "Mocked status"), ctx.json(BooksFixture))),
 
-  rest.get(`${BOOKS_URL}/1`, (req, res, ctx) => res(ctx.status(200, "Mocked status"), ctx.json(BookFixture))),
-
-  rest.get(`${BOOKS_URL}/2`, (req, res, ctx) => res(ctx.status(200, "Mocked status"), ctx.json(BookFixture2))),
+  rest.get(`${BOOKS_URL}/*`, (req, res, ctx) => res(ctx.status(200, "Mocked status"), ctx.json(BooksFixture.books[1]))),
 ];
