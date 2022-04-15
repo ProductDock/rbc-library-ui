ff9519c55757d72fe60098343ea52cdb868f5ff8
vukomanv-pd <100684516+vukomanv-pd@users.noreply.github.com>
2022-04-15 14:13:07 +0200

    Enabled automated deployment of the frontend app

---
+++ prod-env/update-frontend-image.sh
@@ -0,0 +1,9 @@
+                                                                                              update-frontend-image.sh                                                                                                            
+#!/bin/bash
+sudo usermod -a -G docker ${USER}
+docker-credential-gcr configure-docker
+docker stop rbc-library-ui
+docker rm rbc-library-ui
+docker rmi $(docker images | grep "rbc-library-ui")
+docker run -dp 80:80 --name=rbc-library-ui gcr.io/prod-pd-library/rbc-library-ui:$1
+docker container ls -a
