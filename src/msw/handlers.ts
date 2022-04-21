636c905ffdef0ea3f95d5e7f4dd73ed740db3b24
nenadbecanovicpd <nenad.becanovic@productdock.com>
2022-04-21 14:45:17 +0200

    Edit endpoints in tests

--- src/msw/handlers.ts
+++ src/msw/handlers.ts
@@ -1,11 +1,12 @@
 import { rest } from "msw";
 import { BooksFixture } from "./fixtures";
 
-export const BOOKS_URL = `*/books`;
+export const BOOKS_URL = `*/search`;
+export const BOOK_DETAILS_URL = `*/catalog/books`;
 
 export const handlers = [
 
   rest.get(BOOKS_URL, (req, res, ctx) => res(ctx.status(200, "Mocked status"), ctx.json(BooksFixture))),
 
-  rest.get(`${BOOKS_URL}/*`, (req, res, ctx) => res(ctx.status(200, "Mocked status"), ctx.json(BooksFixture.books[1]))),
+  rest.get(`${BOOK_DETAILS_URL}/*`, (req, res, ctx) => res(ctx.status(200, "Mocked status"), ctx.json(BooksFixture.books[1]))),
 ];
