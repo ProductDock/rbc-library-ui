9f264a0a0e37da3deb455c1cfbb48a0c0f3263fa
Dusan <dusan.petrovic@productdock.com>
2022-05-17 15:57:44 +0200

    Added navigation on logo click

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,14 +1,21 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/interactive-supports-focus */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { AppBar, Typography } from "@mui/material";
+import { useNavigate } from "react-router";
+import { routes } from "../../constants/routes";
 import logo from "../../img/pd-logo.svg";
 import AccountAvatar from "../AccountAvatar";
 
 import "./NavBar.css";
 
 const NavBar = () => {
+  const navigate = useNavigate();
+
   return (
     <AppBar className="navbar">
       <div className="navbar-div">
-        <div className="navbar-info">
+        <div className="navbar-info" onClick={() => navigate(routes.HOME)}>
           <img src={logo} alt="logo" className="logo" />
           <div className="navbar-text">
             <span className="navbar-company">
