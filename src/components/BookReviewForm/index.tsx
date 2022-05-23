ff8247a6ce70644f1a9163868499f562aef247bc
vukomanv-pd <100684516+vukomanv-pd@users.noreply.github.com>
2022-05-23 09:54:45 +0200

    Fixed leave a review screen sizing on mobile

--- src/components/BookReviewForm/index.tsx
+++ src/components/BookReviewForm/index.tsx
@@ -46,26 +46,28 @@ const BookReviewForm = ({ onSkip, onSuccessCallback }: Props) => {
 
   return (
     <>
-      <BookReviewFormTitle />
-      <Typography className="book-review-field-title">
-        How would you rate your experience with this book?
-      </Typography>
-      <Rating
-        data-testid="book-review-rating"
-        className="book-review-field"
-        value={rating}
-        onChange={(event, newValue) => setRating(newValue)}
-      />
-      <Typography className="book-review-field-title">
-        To whom would you recommend this book?
-      </Typography>
-      <CheckboxGroup
-        checkboxes={RecommendationCheckboxValues.get()}
-        setCheckedValues={setRecommendation}
-        checkedValues={recommendation}
-      />
-      <Typography className="book-review-field-title">Comment</Typography>
-      <TextArea maxLength={500} text={comment} setText={setComment} />
+      <div className="field-container">
+        <BookReviewFormTitle />
+        <Typography className="book-review-field-title">
+          How would you rate your experience with this book?
+        </Typography>
+        <Rating
+          data-testid="book-review-rating"
+          className="book-review-field"
+          value={rating}
+          onChange={(event, newValue) => setRating(newValue)}
+        />
+        <Typography className="book-review-field-title">
+          To whom would you recommend this book?
+        </Typography>
+        <CheckboxGroup
+          checkboxes={RecommendationCheckboxValues.get()}
+          setCheckedValues={setRecommendation}
+          checkedValues={recommendation}
+        />
+        <Typography className="book-review-field-title">Comment</Typography>
+        <TextArea maxLength={500} text={comment} setText={setComment} />
+      </div>
 
       <div className="book-review-form-footer">
         <SubmitReviewButton
