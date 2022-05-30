49d8dd22d0c70c8dfa166f78d9a753e47216e6e2
dusanpetrovicpd <100684501+dusanpetrovicpd@users.noreply.github.com>
2022-05-30 10:03:46 +0200

    Show no rating text when book has no rating(#72)

--- src/components/BookStarRating/index.tsx
+++ src/components/BookStarRating/index.tsx
@@ -1,5 +1,6 @@
 import { Typography } from "@mui/material";
 import StarIcon from "@mui/icons-material/Star";
+import StarBorderRoundedIcon from "@mui/icons-material/StarBorderRounded";
 
 import "./BookStarRating.css";
 
@@ -12,18 +13,32 @@ type Props = {
 const BookStarRating = ({ rating, ratingsCount, ratingsCountShow }: Props) => {
   return (
     <div className="star-rating">
-      <StarIcon />
-      <Typography fontSize={16} fontWeight={500}>
-        {rating || 0}
-      </Typography>
-      <Typography className="max-rating" fontSize={12} fontWeight={300}>
-        /5
-      </Typography>
-      {ratingsCountShow && (
-        <Typography className="ratings-count" fontSize={12} fontWeight={300}>
-          ({ratingsCount || 0} ratings)
-        </Typography>
+      {rating ? (
+        <StarIcon className="yellow-icon" />
+      ) : (
+        <StarBorderRoundedIcon className="outlined-icon" />
       )}
+
+      {rating && (
+        <>
+          <Typography fontSize={16} fontWeight={500}>
+            {rating}
+          </Typography>
+          <Typography className="max-rating" fontSize={12} fontWeight={300}>
+            /5
+          </Typography>
+        </>
+      )}
+      {ratingsCountShow &&
+        (rating ? (
+          <Typography className="ratings-count" fontSize={12} fontWeight={300}>
+            ({ratingsCount} ratings)
+          </Typography>
+        ) : (
+          <Typography className="ratings-count" fontSize={12} fontWeight={300}>
+            (no ratings)
+          </Typography>
+        ))}
     </div>
   );
 };
