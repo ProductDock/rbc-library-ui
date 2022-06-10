c2f77880726e2f865a0df3e955e44a8d1777a376
nenadbecanovicpd <nenad.becanovic@productdock.com>
2022-06-10 14:58:45 +0200

    Edit msw

--- src/msw/handlers.ts
+++ src/msw/handlers.ts
@@ -11,13 +11,13 @@ export const handlers = [
 
   rest.get(BOOKS_URL, (req, res, ctx) => res(ctx.status(200, "Mocked status"), ctx.json(BooksFixture))),
 
-  rest.get(`${BOOK_DETAILS_URL}/1`, (req, res, ctx) => res(ctx.status(200, "Mocked status"), ctx.json(BooksFixture.books[0]))),
+  rest.get(`${BOOK_DETAILS_URL}/1`, (req, res, ctx) => res(ctx.status(200, "Mocked status"), ctx.json(BooksFixture.books[1]))),
 
-  rest.get(`${BOOK_DETAILS_URL}/2`, (req, res, ctx) => res(ctx.status(200, "Mocked status"), ctx.json(BooksFixture.books[1]))),
+  rest.get(`${BOOK_DETAILS_URL}/2`, (req, res, ctx) => res(ctx.status(200, "Mocked status"), ctx.json(BooksFixture.books[2]))),
 
-  rest.get(`${BOOK_DETAILS_URL}/3`, (req, res, ctx) => res(ctx.status(200, "Mocked status"), ctx.json(BooksFixture.books[2]))),
+  rest.get(`${BOOK_DETAILS_URL}/3`, (req, res, ctx) => res(ctx.status(200, "Mocked status"), ctx.json(BooksFixture.books[3]))),
 
-  rest.get(`${BOOK_DETAILS_URL}/4`, (req, res, ctx) => res(ctx.status(200, "Mocked status"), ctx.json(BooksFixture.books[3]))),
+  rest.get(`${BOOK_DETAILS_URL}/4`, (req, res, ctx) => res(ctx.status(200, "Mocked status"), ctx.json(BooksFixture.books[4]))),
 
   rest.post(RENTAL_RECORD_URL, (req, res, ctx) => res(ctx.status(200, "Mocked status"))),
 
