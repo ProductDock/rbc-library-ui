9b00e47192ee1eb5a42c992ad08b2773f26b5bce
vukomanv-pd <vladimir.vukoman@productdock.com>
2022-06-10 14:39:41 +0200

    Implementing delete review

--- src/pages/BookDetailsPage/BookDetails/ReviewSection/ReviewCollection/ReviewCard/index.tsx
+++ src/pages/BookDetailsPage/BookDetails/ReviewSection/ReviewCollection/ReviewCard/index.tsx
@@ -12,8 +12,17 @@ import {
   Review,
 } from "../../../../../../store/books/details/Types";
 import editIcon from "../../../../../../img/icons/edit-icon.svg";
+import deleteIcon from "../../../../../../img/icons/delete-icon.svg";
 import { useBookReviewContext } from "../../../../../../store/books/reviews/BookReviewContext";
 import { BookReviewFormVariant } from "../../../../../../store/books/reviews/Types";
+import { useBookDetailsContext } from "../../../../../../store/books/details/BookDetailsContext";
+import ConfirmationModal, {
+  ConfirmationRefObject,
+} from "../../../../../../components/Modals/ConfirmationModal";
+import { useRef } from "react";
+
+const title = "Return the book";
+const description = "Are you sure you want to return the book?";
 
 type Props = {
   reviewer: string;
@@ -34,6 +43,10 @@ const ReviewCard = ({
 }: Props) => {
   const { userProfile } = useAuthContext();
   const { selectReview, showReviewForm } = useBookReviewContext();
+  const { book } = useBookDetailsContext();
+  const modal = useRef<ConfirmationRefObject>(null);
+  const showModal = () => modal?.current?.showModal?.();
+  const hideModal = () => modal?.current?.hideModal?.();
 
   const isYourReview = reviewerId === userProfile?.email;
 
@@ -51,6 +64,24 @@ const ReviewCard = ({
       comment,
     });
     showReviewForm?.(BookReviewFormVariant.EDIT);
+  };
+
+  const handleDeleteReview = () => {
+    showModal();
+  };
+
+  const onSuccessHandler = () => {
+    hideModal();
+    const review = findExistingReview();
+
+    if (review) {
+      selectReview?.({
+        rating: review.rating,
+        recommendation: review.recommendation,
+        comment: review.comment,
+      });
+      showReviewForm?.(BookReviewFormVariant.EDIT);
+    } else showReviewForm?.(BookReviewFormVariant.CREATE);
   };
 
   return (
@@ -94,21 +125,41 @@ const ReviewCard = ({
       <div className="review-action-container">
         <div className="review-action-container-edit">
           {userProfile?.email === reviewerId ? (
-            <Link
-              className="edit-a-review-button"
-              underline="none"
-              data-testid="edit-a-review-button"
-              onClick={handleEditReview}
-            >
-              <img
-                src={editIcon}
-                alt="editIcon"
-                className="edit-a-review-button-icon"
-              />
-            </Link>
+            <>
+              <Link
+                className="edit-a-review-button"
+                underline="none"
+                data-testid="edit-a-review-button"
+                onClick={handleEditReview}
+              >
+                <img
+                  src={editIcon}
+                  alt="editIcon"
+                  className="edit-a-review-button-icon"
+                />
+              </Link>
+              <Link
+                className="delete-a-review-button"
+                underline="none"
+                data-testid="delete-a-review-button"
+                onClick={handleDeleteReview}
+              >
+                <img
+                  src={deleteIcon}
+                  alt="deleteIcon"
+                  className="delete-a-review-button-icon"
+                />
+              </Link>
+            </>
           ) : undefined}
         </div>
       </div>
+      <ConfirmationModal
+        ref={modal}
+        title={title}
+        description={description}
+        onConfirmation={() => deleteAReview?.(onSuccessHandler)}
+      />
     </div>
   );
 };
