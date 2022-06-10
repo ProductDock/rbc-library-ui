5adf1fb1db3d84dbe1fa3d917d7c85bd6c4c72e1
nenadbecanovicpd <nenad.becanovic@productdock.com>
2022-06-10 14:48:46 +0200

    Add return a book button on desktop

--- src/pages/BookDetailsPage/BookDetails/BookAction/index.tsx
+++ src/pages/BookDetailsPage/BookDetails/BookAction/index.tsx
@@ -1,3 +1,5 @@
+import { useMediaQuery } from "@mui/material";
+import { MediaQueries } from "../../../../constants/mediaQueries";
 import { BookStatus } from "../../../../store/books/status/Types";
 import BookRentAction from "./BookRentAction";
 import BookReturnAction from "./BookReturnAction";
@@ -7,6 +9,10 @@ type Props = {
 };
 
 const BookAction = ({ bookStatus }: Props) => {
+  const isLargeScreen = useMediaQuery(MediaQueries.X_MEDIUM);
+  if (isLargeScreen && bookStatus === BookStatus.AVAILABLE) {
+    return null;
+  }
   if (bookStatus === BookStatus.AVAILABLE) return <BookRentAction />;
   if (bookStatus === BookStatus.RENTED_BY_YOU) return <BookReturnAction />;
 
