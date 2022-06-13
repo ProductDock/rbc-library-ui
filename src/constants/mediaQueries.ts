ae6cc65733757cec813ebcea34ce6c7db5f1ddcb
NenadBecanovicPD <100684660+NenadBecanovicPD@users.noreply.github.com>
2022-06-13 09:17:17 +0200

    Add return a book button on book details page on desktop

--- src/constants/mediaQueries.ts
+++ src/constants/mediaQueries.ts
@@ -3,6 +3,7 @@ export enum MediaQueries {
     X_SMALL = '(min-width: 320px)',
     SMALL = '(min-width: 480px)',
     MEDIUM = '(min-width: 640px)',
+    X_MEDIUM = '(min-width: 800px)',
     LARGE = '(min-width: 1024px)',
     X_LARGE = '(min-width: 1170px)',
     XX_LARGE = '(min-width: 1440px)'
