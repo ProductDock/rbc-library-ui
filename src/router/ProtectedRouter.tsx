caf3836e412e1e09a942d164e70cf90f789be654
Nenad Jeckovic <nenad.jeckovic@Jeckovics-MacBook-Pro.local>
2024-02-08 10:28:22 +0100

    fix admin routing protection

--- src/router/ProtectedRouter.tsx
+++ src/router/ProtectedRouter.tsx
@@ -1,11 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import LandingPage from "../pages/LandingPage/LandingPage";
 import { useAuthContext } from "../store/auth/AuthContext";
+import { userRoles } from "../constants/userRoles";
+import { routes } from "../constants/routes";
 
-const ProtectedRouter = () => {
-  const { isLoggedIn } = useAuthContext();
+type Props = {
+  role?: string;
+};
 
-   return isLoggedIn ? <Outlet /> : <LandingPage />;
+const ProtectedRouter = ({ role = userRoles.ROLE_USER }: Props) => {
+  const { isLoggedIn, isUserAdmin } = useAuthContext();
+
+  if (isLoggedIn) {
+    if (role === userRoles.ROLE_ADMIN && !isUserAdmin?.()) {
+      return <Navigate to={routes.HOME} />;
+    }
+    return <Outlet />;
+  }
+
+  return <LandingPage />;
 };
 
 export default ProtectedRouter;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -8,6 +8,7 @@ import BookDetailsPage from "../pages/BookDetailsPage/BookDetailsPage";
 import { useAuthContext } from "../store/auth/AuthContext";
 import BookDetailsQrPage from "../pages/BookDetailsQrPage";
 import AdminHomePage from "../pages/AdminHomePage/AdminHomePage";
+import { userRoles } from "../constants/userRoles";
 
 const PageRouter = () => {
   const { loaded } = useAuthContext();
@@ -27,7 +28,10 @@ const PageRouter = () => {
       <Route path={routes.BOOK_DETAILS_QR} element={<ProtectedRouter />}>
         <Route path={routes.BOOK_DETAILS_QR} element={<BookDetailsQrPage />} />
       </Route>
-      <Route path={routes.ADMIN_HOME} element={<ProtectedRouter />}>
+      <Route
+        path={routes.ADMIN_HOME}
+        element={<ProtectedRouter role={userRoles.ROLE_ADMIN} />}
+      >
         <Route path={routes.ADMIN_HOME} element={<AdminHomePage />} />
       </Route>
     </Routes>
