fc9ff73f0e95b3e32715d32fb63c357b5a9ead6d
Nenad Jeckovic <nenad.jeckovic@productdock.com>
2024-03-26 12:16:41 +0100

    Move subscribe hook to hooks folder

--- src/hooks/useSubscribe.tsx
+++ src/hooks/useSubscribe.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import * as bookService from "../../../../../services/BookService";
+import * as bookService from "../services/BookService";
 
 const useSubscribe = (bookId: number) => {
   const [isSubscribed, setSubscribed] = useState<boolean>();
